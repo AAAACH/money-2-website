47a4d12f6a020d98989f3c9f5b0e15725e5c2bf8
CHEN <812607138@qq.com>
2023-07-19 01:11:44 +0800

    deploy

---
+++ precache-manifest.4cb4eb682ea8afd0ed524cf170d7c28b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b372fd41db831b27df5d",
+    "url": "/money-2-website/css/app.c88b059d.css"
+  },
+  {
+    "revision": "d07870cd3b72eb84e0266e8a4eee5d1f",
+    "url": "/money-2-website/index.html"
+  },
+  {
+    "revision": "b372fd41db831b27df5d",
+    "url": "/money-2-website/js/app.ec2cde28.js"
+  },
+  {
+    "revision": "ca8716e7bcd6569065b0",
+    "url": "/money-2-website/js/chunk-vendors.4c98169c.js"
+  },
+  {
+    "revision": "38223f382ae68db0b789beafa5bf680b",
+    "url": "/money-2-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-2-website/robots.txt"
+  }
+]);
